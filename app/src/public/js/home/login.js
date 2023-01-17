e08e9a2ce24afcb5d85be6539e5e9bbbde5173a6
hmp815 <bakhunm2@gmail.com>
2023-01-17 23:25:43 +0900

    make fetch for request login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,16 @@ function login(){
         id : id.value,
         pw : pw.value,
     };
-    console.log(req);
+
+    // 어떤 경로로 전달을 할지 정해야함, 서버로 데이터 전달, 서버에 해당 데이터를 받을API가 있어야 함.
+    fetch("/login", {
+        // POST : /login을 생성 하라 GET : /login을 조회
+        method : "POST", 
+        //headers : 내가 전달하는 데이터가 어떤 데이터인지 알려주는 역할
+        headers:{
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req) // stringify는 문자열로 변환하는 함수
+    });  
+    //console.log(req);
 }
